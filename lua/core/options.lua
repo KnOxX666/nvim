2b73524bc86e43f6ad2d1d0bf8cbd601e38dc5f0
Marcus Vogelgesang <marcus.vogelgesang@frequentis.com>
2025-01-17 12:06:49 +0100

    made cursor blink

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,3 +41,6 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- cursor blinks
+opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
